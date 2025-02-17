69462ba184b3d21a1740804e486e0d124c19c980
i3bdolah <xbdullahx@gmail.com>
2025-02-17 20:52:36 +0300

    Add project files.

---
+++ Course-16-CSharp-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_16_CSharp_2
+{
+    internal class Program
+    {
+        class clsPerson
+        {
+            // Fields
+            public bool IsSaeed;
+            public string FirstName;
+            public string LastName;
+
+            // Methods
+            public void GetFullName()
+            {
+                System.Console.WriteLine(this.FirstName + " " + this.LastName);
+            }
+        }
+        static void Main(string[] args)
+        {
+            clsPerson Abdullah = new clsPerson();
+
+            Abdullah.IsSaeed = true; // بإذن الله أكون من السعداء
+            Abdullah.FirstName = "Abdullah";
+            Abdullah.LastName = "Al-Shehri";
+
+            Abdullah.GetFullName();
+        }
+    }
+}

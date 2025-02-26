f6ffb92ad89127d5c079acf6e29ff782d38bffd7
i3bdolah <xbdullahx@gmail.com>
2025-02-26 22:50:22 +0300

    Parameter-less Constructor

---
+++ ConstructorAndDestructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAndDestructor
+{
+
+    class clsEmployee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public clsEmployee()
+        {
+            Id = 1;
+            Name = "Unknown";
+            Description = "Unknown";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsEmployee employee = new clsEmployee();
+            Console.WriteLine(employee.Id);
+            Console.WriteLine(employee.Name);
+            Console.WriteLine(employee.Description);
+        }
+    }
+}

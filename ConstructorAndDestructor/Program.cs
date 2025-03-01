2f381bd238d7478bcb9bb043d190910746f8a04b
i3bdolah <xbdullahx@gmail.com>
2025-03-01 23:09:55 +0300

    Multiple Constructors using overloading

--- ConstructorAndDestructor/Program.cs
+++ ConstructorAndDestructor/Program.cs
@@ -7,44 +7,42 @@ using System.Threading.Tasks;
 namespace ConstructorAndDestructor
 {
 
-    //static class Settings
-    class Settings
+    class clsEmployee
     {
-        public static int DayNumber
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public clsEmployee(int Id, string Name, string Description)
         {
-            get
-            {
-                return DateTime.Today.Day;
-            }
+            this.Id = Id;
+            this.Name = Name;
+            this.Description = Description;
         }
 
-        public static string DayName
+        public clsEmployee()
         {
-            get
-            {
-                return DateTime.Today.DayOfWeek.ToString();
-            }
+            this.Id = 0;
+            this.Name = "Name";
+            this.Description = "Description";
         }
-
-        public static string ProjectPath
-        {
-            get;
-            set;
-        }
-
-        //private Settings()
-        //{
-
-        //}
     }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Settings aa = new Settings();
+            clsEmployee employee1 = new clsEmployee(158, "Abdullah", "Software Engineer");
+            Console.WriteLine(employee1.Id);
+            Console.WriteLine(employee1.Name);
+            Console.WriteLine(employee1.Description);
 
-            Console.WriteLine(Settings.DayNumber);
-            Console.WriteLine(Settings.DayName);
+            Console.WriteLine();
+
+            clsEmployee employee2 = new clsEmployee();
+            Console.WriteLine(employee2.Id);
+            Console.WriteLine(employee2.Name);
+            Console.WriteLine(employee2.Description);
         }
     }
 }

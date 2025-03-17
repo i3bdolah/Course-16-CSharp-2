358921a06240722ee6a88596e98b163417f36cda
i3bdolah <xbdullahx@gmail.com>
2025-03-17 03:56:49 +0300

    Method Overriding

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+
+    class clsA
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Im Class A !");
+        }
+    }
+
+    class clsB : clsA
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Im Class B !");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsA a = new clsB();
+            clsA b = new clsA();
+
+            a.Print();
+            b.Print();
+
+        }
+    }
+}
